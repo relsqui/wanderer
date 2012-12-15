8bd7b47e85cc70a4f3a1c96f5d797ce414a237b1
Fizz <relsqui@chiliahedron.com>
2012-12-15 05:16:35 -0800

    Created sprites.Animation. Closes #8

--- wanderer/modules/sprites.py
+++ wanderer/modules/sprites.py
@@ -5,32 +5,35 @@ class Character(pygame.sprite.Sprite):
     def __init__(self, screen, sheet):
         super(Character, self).__init__()
         self.sheet = sheet
-        self.colorkey = self.sheet.get_at((0,0))
-        self.images = [[x for x in xrange(3)] for x in xrange(4)]
+        self.animations = [0, 1, 2, 3]
+        self.animation = None
         self.init_images()
+
         self.velocity = (0,0)
-        self.direction = DOWN
-        self.position = 1
-        self.walk_timer = 0
-        self.image = self.images[self.direction][self.position]
-        self.image.set_colorkey(self.colorkey)
+        self.speed = SPRITE_SPEED
+
+        self.turn(DOWN)
+        self.image = self.animation.image()
+
         self.area = screen.get_rect()
         self.rect = self.image.get_rect()
         self.rect.center = self.area.center
-        self.speed = SPRITE_SPEED
 
     def init_images(self):
         for direction in (DOWN, LEFT, RIGHT, UP):
-            self.images[direction] = [0, 1, 2, 3]
+            frames = []
             for position in (0, 1, 2):
                 cursor = pygame.Rect(position * SPRITE_WIDTH, direction * SPRITE_HEIGHT, SPRITE_WIDTH, SPRITE_HEIGHT)
-                self.images[direction][position] = self.sheet.subsurface(cursor)
-            self.images[direction][3] = self.images[direction][1]
+                frames.append(self.sheet.subsurface(cursor))
+            frames.append(frames[1])
+            self.animations[direction] = Animation(frames, self.sheet.get_at((0,0)), WALK_RATE)
 
     def update(self, loop_time):
         if self.velocity == (0,0):
+            self.animation.stop()
             self.position = 1
         else:
+            self.animation.start()
             angle = self.velocity
             if self.velocity[0] and self.velocity[1]:
                 if self.direction in (LEFT, RIGHT):
@@ -39,32 +42,28 @@ class Character(pygame.sprite.Sprite):
                     angle = (0, self.velocity[1])
             elif self.velocity[0]:
                 if self.velocity[0] > 0:
-                    self.direction = RIGHT
+                    self.turn(RIGHT)
                 else:
-                    self.direction = LEFT
+                    self.turn(LEFT)
             elif self.velocity[1]:
                 if self.velocity[1] > 0:
-                    self.direction = DOWN
+                    self.turn(DOWN)
                 else:
-                    self.direction = UP
+                    self.turn(UP)
             newpos = self.rect.move(angle)
             if newpos.left > self.area.left and newpos.right < self.area.right and newpos.top > self.area.top and newpos.bottom < self.area.bottom:
                 self.rect = newpos
-            if self.walk_timer:
-                self.walk_timer -= loop_time
-                if self.walk_timer < 0:
-                    self.walk_timer = 0
-            else:
-                self.position = (self.position + 1) % 4
-                self.walk_timer = WALK_RATE
 
-        self.image = self.images[self.direction][self.position]
-        self.image.set_colorkey(self.colorkey)
+        self.image = self.animation.image(loop_time)
 
 
     def move(self, direction):
-        self.direction = direction
+        self.turn(direction)
         self.accelerate(direction)
+
+    def turn(self, direction):
+        self.direction = direction
+        self.animation = self.animations[direction]
 
     def accelerate(self, direction, to_stop = False):
         xvel, yvel = self.velocity
@@ -85,3 +84,36 @@ class Character(pygame.sprite.Sprite):
         yvel = min(yvel, self.speed)
         yvel = max(yvel, -1 * self.speed)
         self.velocity = (xvel, yvel)
+
+
+class Animation(object):
+    def __init__(self, frames, colorkey, speed, default = 1):
+        super(Animation, self).__init__()
+        self.frames = frames
+        self.colorkey = colorkey
+        self.speed = speed
+        self.default = default
+        self.current = default
+        self.countdown = speed
+        self.running = False
+
+    def update(self, loop_time):
+        if self.running:
+            self.countdown -= loop_time
+            if self.countdown <= 0:
+                self.current = (self.current + 1) % len(self.frames)
+                self.countdown = self.speed
+
+    def image(self, loop_time = 0):
+        if loop_time:
+            self.update(loop_time)
+        image = self.frames[self.current]
+        image.set_colorkey(self.colorkey)
+        return image
+
+    def start(self):
+        self.running = True
+
+    def stop(self):
+        self.running = False
+        self.current = self.default
