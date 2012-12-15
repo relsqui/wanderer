250c5d41a656156c5094f9ef93ba9a8ad2e25b2f
Fizz <relsqui@chiliahedron.com>
2012-12-14 19:56:04 -0800

    Particles, this time with files.

---
+++ exploring_turtle/modules/particles.py
@@ -0,0 +1,41 @@
+import pygame
+from modules.constants import *
+from modules import game
+
+class Particle(pygame.sprite.Sprite):
+    """
+    A temporary sprite which will disappear after a short time. Arguments:
+        surface (pygames.Surface)
+        location (pygames.Rect)
+        timeout (integer, milliseconds)
+        fade (integer)
+    """
+
+    def __init__(self, surface, location, timeout = PARTICLE_DEFAULT_TIMEOUT, fade = PARTICLE_DEFAULT_FADE):
+        super(Particle, self).__init__()
+        self.fade = fade
+        self.timeout = timeout
+        self.countdown = self.timeout
+        self.contents = surface
+
+    def update(self):
+        self.countdown -= game.clock.get_time()
+        if self.countdown < 0:
+            self.kill()
+
+
+class TextParticle(Particle):
+    """
+    A particle of rendered text. Arguments:
+        message (string)
+        destination (pygames.Rect)
+        timeout (integer, milliseconds)
+        fade (integer)
+    """
+
+    def __init__(self, message, destination, timeout = PARTICLE_DEFAULT_TIMEOUT, fade = PARTICLE_DEFAULT_FADE):
+        text = game.font.render(message, False, TEXT_COLOR)
+        textpos = text.get_rect()
+        textpos.centerx = destination.centerx
+        textpos.centery = destination.centery
+        super(TextParticle, self).__init__(text, textpos, timeout, fade)
